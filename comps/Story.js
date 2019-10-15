a41dcbb8a3c5da77d26da0bcaf5994b433a7b2a4
Doris <doris.h@live.ca>
2019-10-15 11:33:34 -0700

    Got the thing to work
    I got the colour change to work

--- comps/Story.js
+++ comps/Story.js
@@ -1,5 +1,6 @@
 import React,{useState} from 'react';
 import {
+    Text,
     View, 
     Switch,
     ScrollView
@@ -12,29 +13,29 @@ function Story(){
 
     var colorTheme = null
 
-    if(BGColor === true){
+    if(BGColor === false){
         colorTheme = (
-            <View style={[styles.cont,{backgroundColor:'powderblue'}]}
-            >
+            <View style={[styles.cont,{backgroundColor:'powderblue'}]}>
+                <Text> Night Mode </Text>
                 <Switch
                 value = {BGColor}
-                onValueChange={(itemValue) => setBGColor(itemValue)}>
-                </Switch>
-                <Text style={[styles.cont,{textColor:'#000'}]}> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                onValueChange={(itemValue) => setBGColor(itemValue)}/>
+                <Text style={[styles.text,{color:'#000'}], styles.textBox}> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
                 </Text>
             </View>
         )
     }
 
-    if(BGColor === false){
+    if(BGColor === true){
         colorTheme = (
-            <View style={[style.cont,{backgroundColor:'black'}]}>
+            <View style={[styles.cont,{backgroundColor:'black'}]}>
+                 <Text style={[styles.text,{color:'#FFF'}]}> Night Mode </Text>
                 <Switch
                 value = {BGColor}
                 onValueChange={(itemValue) => setBGColor(itemValue)}>
 
                 </Switch>
-                <Text style={[styles.cont,{textColor:'#FFF'}]}> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                <Text style={[styles.text,{color:'#FFF'}]}> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
 
         </Text>
             </View>
--- styles/StoryStyles.js
+++ styles/StoryStyles.js
@@ -4,6 +4,11 @@ var styles = StyleSheet.create({
     cont:{
         height: "100%",
         fontSize: 14,
+        padding: 20,
+    },
+
+    textbox:{
+        lineHeight: 25
     }
 })
 
